2164d16abc5de6534e59435d3e9702f48d273154
proxigo35 <andrei.t35@outlook.com>
2025-12-12 12:32:10 +0000

    Changed functionality so that HTTP request happens inside of home page. If results are returned, a div with the recipes is added to the DOM.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,25 +1,62 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { environment } from '../../environments/environment';
 import { Data } from '../services/data';
+import { Http } from '../services/http';
+import { HttpOptions } from '@capacitor/core';
 import { Router } from '@angular/router';
-import { IonButton, IonInput, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonInput,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonCardTitle,
+  IonCardHeader,
+  IonCard,
+  IonContent,
+} from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [FormsModule, IonButton, IonInput, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    CommonModule,
+    IonCardTitle,
+    IonCardHeader,
+    IonCard,
+    FormsModule,
+    IonButton,
+    IonInput,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+  ],
 })
 export class HomePage {
-	
-  ingredients: string = "";
-  
-  constructor(private router: Router, private ds: Data) {}
-  
-  async openRecipes() {
-    await this.ds.set("ingredients", this.ingredients);
-	this.router.navigate(['/recipes']);
+  ingredients: string = '';
+  recipes: any[] = [];
+  searchInitiated: boolean = false;
+
+  options: HttpOptions = {
+    url: ''
+  };
+
+  constructor(
+    private ds: Data,
+    private mhs: Http,
+  ) {}
+
+  async getRecipes() {
+    this.options.url =
+      'https://api.spoonacular.com/recipes/complexSearch?apiKey=' +
+      environment.apiKey +
+      '&query=' +
+      this.ingredients;
+    this.recipes = (await this.mhs.get(this.options)).data.results;
+    this.searchInitiated = true;
   }
-	
-  
 }
